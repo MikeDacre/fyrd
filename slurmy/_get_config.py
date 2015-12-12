8e128bdd8ad6b4253f1c70716e6e405ff6099c86
Mike Dacre <mike.dacre@gmail.com>
2015-12-11 22:43:08 -0800

    Added ability to write new or updated config to config file

--- slurmy/_get_config.py
+++ slurmy/_get_config.py
@@ -2,7 +2,7 @@
 Description:   Get and set global variables
 
 Created:       2015-12-11
-Last modified: 2015-12-11 22:16
+Last modified: 2015-12-11 22:42
 """
 from os import path
 from os import environ
@@ -15,8 +15,18 @@ config      = configparser.ConfigParser()
 defaults    = {}
 
 
+def _config_to_dict(config):
+    """ Convert a config object into a dictionary """
+    for section in config.sections():
+        defaults[section] = {}
+        for k, v in config[section].items():
+            defaults[section][k] = v
+    return defaults
+
+
 def _create_config():
     """ Create a ~/.slurmy file """
+    global defaults, config
     defaults['queue'] = {'max_jobs':     1000,  # Max number of jobs in queue
                          'sleep_len':    5,     # Between submission attempts (in seconds)
                          'queue_update': 20}    # Amount of time between getting fresh queue info (seconds)
@@ -31,14 +41,20 @@ def _create_config():
 
 def _get_config():
     """ Load defaults from ~/.slurmy """
-    global defaults
+    global defaults, config
     if path.isfile(config_file):
         config.read(config_file)
-        for section in config.sections():
-            defaults[section] = {}
-            for k, v in config[section].items():
-                defaults[section][k] = v
+        defaults = _config_to_dict(config)
     else:
-        # Make the config file
         defaults = _create_config()
     return defaults
+
+
+def write_config(section, key, value):
+    """ Write a config key to the ~/.slurmy file """
+    global defaults, config
+    config.read(config_file)
+    config[section][key] = value
+    with open(config_file, 'w') as outfile:
+        config.write(outfile)
+    return _get_config()
